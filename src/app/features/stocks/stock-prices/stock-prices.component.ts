66116ddc159ba4f644ea0ff13434e68a5e11dea4
WojciechSnopek <wojciechsnopek2@gmail.com>
2019-10-27 12:57:37 +0100

    Added tables with data from service

--- src/app/features/stocks/stock-prices/stock-prices.component.ts
+++ src/app/features/stocks/stock-prices/stock-prices.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { StockGatewayService } from '../../../core/services/stock-gateway.service';
+import { Stock } from 'src/app/core/entities/stock.entity';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-stock-prices',
   templateUrl: './stock-prices.component.html',
-  styleUrls: ['./stock-prices.component.scss']
+  styleUrls: ['./stock-prices.component.scss'],
+  providers: [StockGatewayService]
 })
 export class StockPricesComponent implements OnInit {
+  public stocks$: Observable<Array<Stock>>;
+  displayedColumns: string[] = ['company', 'value', 'actions'];
 
-  constructor(private readonly stockdataService: StockGatewayService) { }
+  constructor(private readonly stockdataService: StockGatewayService) {
+    this.getStockData();
+   }
 
   ngOnInit() {
-    // this.stockdataService.getStocks$().subscribe(stocks => console.log(stocks));
+  }
+
+  private getStockData() {
+    this.stocks$ = this.stockdataService.getStocks$();
   }
 
 }
--- src/app/features/wallet/my-wallet/my-wallet.component.ts
+++ src/app/features/wallet/my-wallet/my-wallet.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { StockGatewayService } from 'src/app/core/services/stock-gateway.service';
+import { Stock } from 'src/app/core/entities/stock.entity';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-my-wallet',
   templateUrl: './my-wallet.component.html',
-  styleUrls: ['./my-wallet.component.scss']
+  styleUrls: ['./my-wallet.component.scss'],
+  providers: [StockGatewayService]
 })
 export class MyWalletComponent implements OnInit {
+public stocks$: Observable<Array<Stock>>;
+displayedColumns: string[] = ['company', 'value', 'unit', 'totalcost', 'actions'];
 
-  constructor() { }
+  constructor(private readonly stockDataService: StockGatewayService) {
+    this.getStockData();
+  }
 
   ngOnInit() {
   }
 
+  private getStockData() {
+      this.stocks$ = this.stockDataService.getStocks$();
+  }
 }
